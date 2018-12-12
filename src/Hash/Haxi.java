b7893806bf2fa901665f0aa83d493fb06588b225
jungletada <1002955176@qq.com>
2018-12-12 11:29:29 +0800

    Hash

---
+++ src/Hash/Haxi.java
@@ -0,0 +1,365 @@
+
+package Hash;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class Haxi {
+    public int Czy=1;//
+    public int len;//
+    public final int size=50;//表长
+
+    public Hash Hashtable_phone;//电话号码哈希表
+    public Hash Hashtable_name;//姓名哈希表
+    public Hash Hashtable_address;//地址哈希表
+
+    public UserData []record;//用户的信息，线性结构存储
+
+    public String Res_phone;//电话号码搜索结果
+    public String Res_name;//姓名搜索结果
+    public String Res_address;//地址搜索结果
+
+    public int phone_key;//电话号码获得的key
+    public int name_key;//姓名获得的key
+    public int address_key;//地址获得的key
+
+    //初始的电话簿文件 TeleTable.txt
+    public final String TeleTable="C:\\java\\HashFrame\\src\\Hash\\TeleTable.txt";
+    public File file;
+    public BufferedReader reader = null;
+
+    /**
+     * 构造函数
+     * 定义及初始化
+     * 电话号码哈希表初始化
+     * 地址哈希表初始化
+     * 姓名哈希表初始化
+     */
+    public Haxi(){
+        file=new File(TeleTable);
+        Hashtable_phone = new Hash();
+        Hashtable_name =new Hash();
+        Hashtable_address = new Hash();
+
+        record = new UserData[size];
+
+        for(int i=0;i<size;i++) {
+            record[i]=new UserData();
+        }
+
+        Hashtable_phone.data=new UserData[size];
+
+        for(int i=0;i<size;i++) {
+            Hashtable_phone.data[i]=new UserData();
+        }
+        Hashtable_phone.size=size;
+        Hashtable_phone.cnt=0;
+
+        Hashtable_address.data=new UserData[size];
+        for(int i=0;i<size;i++) {
+            Hashtable_address.data[i]=new UserData();
+        }
+        Hashtable_address.size=size;
+        Hashtable_address.cnt=0;
+
+        Hashtable_name.data=new UserData[size];
+        for(int i=0;i<size;i++) {
+            Hashtable_name.data[i]=new UserData();
+        }
+        Hashtable_name.size=size;
+        Hashtable_name.cnt=0;
+    }
+
+    /**
+     * 输入资料,直接从文件读入
+     */
+    public void InitialTable() {
+        try{
+            reader = new BufferedReader(new FileReader(file));
+            String temp;
+            int i=0,times=1;
+            while ((temp=reader.readLine())!=null) {
+                if(times%3==1){
+                    record[i].phoneNumber=temp;
+                }
+                else if(times%3==2){
+                    record[i].name=temp;
+                }
+                else{
+                    record[i].address=temp;
+                    i++;
+                }
+                times++;
+            }
+            len=i;
+            reader.close();
+        }catch (IOException e){}
+
+        /**
+         * 创建哈希表
+         */
+        CreateHashTable_Phone();
+        CreateHashTable_Name();
+        CreateHashTable_Address();
+        /**
+         * 输出
+         */
+        for(int i=0;i<size;i++) {
+            System.out.println(i+" "+Hashtable_address.data[i].phoneNumber+" "
+                    +Hashtable_address.data[i].name+" "+Hashtable_address.data[i].address);
+        }
+    }
+
+    /**根据电话号码获得key
+     * @param phone
+     * @return
+     */
+    public int GetHashKey_Phone(String phone) {
+        int len = phone.length();
+        int key=0;
+        for(int i=0;i<len;i++) {
+            int temp=Integer.parseInt(String.valueOf(phone.charAt(i)));
+            key+=temp;
+        }
+        return key%size;
+    }
+    /**根据姓名获得key
+     * @param Name
+     * @return
+     */
+    public int GetHashKey_Name(String Name) {
+        int len=Name.length();
+        int key=0;
+        for(int i=0;i<len;i++) {
+            int temp=(int)Name.charAt(i);
+            key+=temp;
+        }
+        return key%size;
+    }
+    /**
+     * 根据地址获得key
+     * @param Address
+     * @return
+     */
+    public int GetHashKey_Address(String Address) {
+        int len=Address.length();
+        int key=0;
+        for(int i=0;i<len;i++) {
+            //char t=phone.charAt(i);
+            int temp=(int)Address.charAt(i);
+            key+=temp;
+        }
+        return key%size;
+    }
+
+    /**
+     *根据电话号码构建哈希表
+     */
+    public void CreateHashTable_Phone() {
+        for(int i=0;i<len;i++){
+            phone_key=GetHashKey_Phone(record[i].phoneNumber);
+            if(Hashtable_phone.data[phone_key].name.charAt(0)!='0')
+                phone_key=HandleCollission(Hashtable_phone,phone_key);
+
+            Hashtable_phone.data[phone_key].phoneNumber=record[i].phoneNumber;
+            Hashtable_phone.data[phone_key].name=record[i].name;
+            Hashtable_phone.data[phone_key].address=record[i].address;
+        }
+    }
+    /**
+     *根据姓名构建哈希表
+     */
+    public void CreateHashTable_Name() {
+        for(int i=0;i<len;i++){
+            name_key=GetHashKey_Name(record[i].name);
+            if(Hashtable_name.data[name_key].name.charAt(0)!='0')
+                name_key=HandleCollission(Hashtable_name,name_key);
+
+            Hashtable_name.data[name_key].phoneNumber=record[i].phoneNumber;
+            Hashtable_name.data[name_key].name=record[i].name;
+            Hashtable_name.data[name_key].address=record[i].address;
+        }
+    }
+
+    /**
+     *根据地址构建哈希表
+     */
+    public void CreateHashTable_Address() {
+        for(int i=0;i<len;i++){
+            address_key = GetHashKey_Address(record[i].address);
+            if(Hashtable_address.data[address_key].name.charAt(0)!='0')
+                address_key=HandleCollission(Hashtable_address,address_key);
+
+            Hashtable_address.data[address_key].phoneNumber=record[i].phoneNumber;
+            Hashtable_address.data[address_key].name=record[i].name;
+            Hashtable_address.data[address_key].address=record[i].address;
+        }
+    }
+
+    /**
+     * 冲突处理，二次探测再散列
+     * @param Hashtable
+     * @param key
+     * @return
+     */
+    public int HandleCollission(Hash Hashtable,int key) {
+        Czy=1; //从2,3,4,5,.......
+        while(true){
+            Czy++; //从2,3,4,5,.......
+            if(Czy%2==0) {
+                if(Hashtable.data[(key+(Czy/2)*(Czy/2))%50].name.charAt(0)=='0')
+                    return (key+(Czy/2)*(Czy/2))%50;
+            }
+            else if(Czy%2!=0) {
+                if((key-(Czy/2)*(Czy/2))<0)
+                    continue;//由于是减法，要注意负数不能取模
+                if(Hashtable.data[(key-(Czy/2)*(Czy/2))%50].name.charAt(0)=='0')
+                    return (key-(Czy/2)*(Czy/2))%50;
+            }
+        }
+    }
+    /**
+     * 根据电话号码查询
+     * @param PhoneNumber
+     */
+    public boolean SerchKey_Phone(String PhoneNumber) {
+        phone_key=GetHashKey_Phone(PhoneNumber);
+        if(!Hashtable_phone.data[phone_key].phoneNumber.equals(PhoneNumber)){
+            for(Czy=1;Czy<50;Czy++){
+                if(Czy%2==0) {
+                    if(PhoneNumber.equals(Hashtable_phone.data[(phone_key+(Czy/2)*(Czy/2))%50].phoneNumber)){
+                        phone_key= (phone_key+(Czy/2)*(Czy/2))%50;
+                        break;
+                    }
+                }
+                else if(Czy%2!=0) {
+                    if((phone_key-(Czy/2)*(Czy/2))<0) continue;//由于是减法，要注意负数不能取模
+                    if(PhoneNumber.equals(Hashtable_phone.data[(phone_key-(Czy/2)*(Czy/2))%50].phoneNumber)){
+                        phone_key= (phone_key-(Czy/2)*(Czy/2))%50;
+                        break;
+                    }
+                }
+            }
+        }
+        if(Hashtable_phone.data[phone_key].address.charAt(0)!='0'){
+            Res_phone=Hashtable_phone.data[phone_key].name+"--"+Hashtable_phone.data[phone_key].phoneNumber
+                +"--"+Hashtable_phone.data[phone_key].address;
+            System.out.println(Res_phone);
+            return true;
+        }
+        else{
+            Res_name="该号码不在名单上";
+            System.out.println(Res_name);
+            return false;
+        }
+    }
+
+    /**
+     * 根据姓名查询
+     * @param Name
+     * @return
+     */
+    public boolean SerchKey_Name(String Name)
+    {
+        name_key=GetHashKey_Name(Name);
+        if(!Hashtable_name.data[name_key].name.equals(Name)){
+            for(Czy=1;Czy<50;Czy++){
+                if(Czy%2==0) {
+                    if(Name.equals(Hashtable_name.data[(name_key+(Czy/2)*(Czy/2))%50].name)){
+                        name_key= (name_key+(Czy/2)*(Czy/2))%50;
+                        break;
+                    }
+                }
+                else if(Czy%2!=0) {
+                    if((name_key-(Czy/2)*(Czy/2))<0) continue;//由于是减法，要注意负数不能取模
+                    if(Name.equals(Hashtable_name.data[(name_key-(Czy/2)*(Czy/2))%50].name)){
+                        name_key= (name_key-(Czy/2)*(Czy/2))%50;
+                        break;
+                    }
+                }
+            }
+        }
+        if(Hashtable_name.data[name_key].name.charAt(0)!='0'){
+            Res_name=Hashtable_name.data[name_key].name+"--"+Hashtable_name.data[name_key].phoneNumber
+                    +"--"+Hashtable_name.data[name_key].address;
+            System.out.println(Res_name);
+            return true;
+        }
+        else{
+            Res_name="该名字不在名单上";
+            System.out.println(Res_name);
+            return false;
+        }
+    }
+
+    /**
+     * 根据地址查询
+     * @param Address
+     * @return
+     */
+    public boolean SerchKey_Address(String Address)
+    {
+        address_key =GetHashKey_Address(Address);
+        if(!Hashtable_address.data[address_key].address.equals(Address)){
+            for(Czy=1;Czy<50;Czy++){
+                if(Czy%2==0) {
+                    if(Address.equals(Hashtable_address.data[(address_key +(Czy/2)*(Czy/2))%50].address)){
+                        address_key = (address_key +(Czy/2)*(Czy/2))%50;
+                        break;
+                    }
+                }
+                else if(Czy%2!=0) {
+                    if((address_key -(Czy/2)*(Czy/2))<0) continue;//由于是减法，要注意负数不能取模
+                    if(Address.equals(Hashtable_address.data[(address_key -(Czy/2)*
+                            (Czy/2))%50].address)){
+                        address_key = (address_key -(Czy/2)*(Czy/2))%50;
+                        break;
+                    }
+                }
+            }
+        }
+        if(Hashtable_address.data[address_key].name.charAt(0)!='0'){
+        Res_address =Hashtable_address.data[address_key].name+"--"+
+                Hashtable_address.data[address_key].phoneNumber
+                +"--"+Hashtable_address.data[address_key].address;
+        System.out.println(Res_address);
+        return true;
+        }
+        else{
+            Res_name="该地址不在名单上";
+            System.out.println(Res_address);
+            return false;
+        }
+    }
+}
+
+/**
+ * 用户数据，作为一个类
+ * 初始时全部作为‘0’
+ * @Haxi
+ */
+class UserData{
+    String phoneNumber;
+    String name;
+    String address;
+    UserData(){
+        phoneNumber="0";
+        name="0";
+        address="0";
+    }
+}
+
+/**哈希表，作为一个线性结构
+ * 存储表长
+ * @UserData
+ * @Haxi
+ */
+class Hash{
+    UserData data[];
+    int cnt;
+    int size;
+}
+
+
---
+++ src/Hash/MyTextField.java
@@ -0,0 +1,289 @@
+package Hash;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.border.MatteBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * 继承JPanel作为界面，设置监听
+ * @Haxi
+ */
+class GraPanel extends JPanel implements ActionListener{
+    public BufferedImage back;//背景图片
+    public JButton b1,b2,b3,b4,b5;//按钮
+
+    public JTextField textField,f1,f2,result;//文本区域
+    public JTextField Textname,Textadress,Textnum;//文本区域
+    public JTextField Tname,Tadress,Tnum;//文本区域
+
+    public String PhoneNumber;//输入的电话号码
+    public String Name;//输入的姓名
+    public String Address;//输入的地址
+    public Haxi haxi;//总哈希表
+
+    public GraPanel(){
+        haxi=new Haxi();
+        try{
+            back = ImageIO.read(MyTextField.class.getResource("BACK.jpg"));
+        }catch (IOException e){}
+        setButton();
+        setTextField();
+        this.add(f1);
+        this.add(textField,BorderLayout.NORTH);
+        this.add(b1);
+        this.add(b2);
+        this.add(b3);
+        this.add(f2);
+        this.add(result);
+        this.add(b4);
+        this.add(b5);
+    }
+
+    public void SearchByName(){
+        boolean flag=true;
+        if(Name.length()>20){
+            flag=false;
+        }
+        if(Name.contains("!") || Name.contains("@")||Name.contains("#")||
+                Name.contains("$")||Name.contains("%")){
+            flag=false;
+        }
+        for(int i=0;i<Name.length();i++){
+            if(Name.charAt(i)>=0&&Name.charAt(i)<=9){
+                flag=false;
+                break;
+            }
+        }
+        if(!flag){
+            JOptionPane.showMessageDialog(null,
+                    "请输入正确的姓名!","WARNING",JOptionPane.WARNING_MESSAGE);
+            System.out.println("Error name!");
+        }
+    }
+
+    public void SearchByNum(){
+        boolean flag=true;
+        if(PhoneNumber.length()>11){
+            flag=false;
+        }
+        for(int i=0;i<PhoneNumber.length();i++){
+            if(PhoneNumber.charAt(i)<'0' || PhoneNumber.charAt(i)>'9'){
+                flag=false;
+                break;
+            }
+        }
+        if(!flag){
+            JOptionPane.showMessageDialog(null,
+                    "请输入正确的电话号码!","WARNING",JOptionPane.WARNING_MESSAGE);
+            System.out.println("Error telephone number!");
+        }
+    }
+
+    /**
+     * 设置按钮
+     */
+    public void setButton(){
+        Font font1=new Font("黑体",Font.BOLD,28);
+        Font font2=new Font("黑体",Font.BOLD,28);
+        MatteBorder border2=
+                new MatteBorder(3,3,3,3,new Color(63, 55, 224));
+        MatteBorder border=
+                new MatteBorder(3,3,3,3,new Color(149, 8, 224));
+        MatteBorder border3=
+                new MatteBorder(3,3,3,3,new Color(101, 25, 224));
+
+        b1=new JButton("按电话号码查询");
+        b1.setFont(font1);
+        b1.setPreferredSize(new Dimension(500,70));
+        b1.setBackground(new Color(55, 201, 245));
+        b1.setBorder(border);
+
+        b2=new JButton("按姓名查询");
+        b2.setPreferredSize(new Dimension(500,70));
+        b2.setFont(font1);
+        b2.setBackground(new Color(196, 147, 245));
+        b2.setBorder(border2);
+        b2.setOpaque(true);
+
+        b3=new JButton("按地址查询");
+        b3.setPreferredSize(new Dimension(500,70));
+        b3.setFont(font1);
+        b3.setBackground(new Color(245, 157, 144));
+        b3.setBorder(border2);
+        b3.setOpaque(true);
+
+        b4=new JButton("添加");
+        b4.setPreferredSize(new Dimension(500,70));
+        b4.setFont(font1);
+        b4.setBackground(new Color(245, 197, 74));
+        b4.setBorder(border2);
+        b4.setOpaque(true);
+
+        b5=new JButton("退出");
+        b5.setFont(font2);
+        b5.setBackground(new Color(245, 244, 145));
+        b5.setBorder(border3);
+        b5.setPreferredSize(new Dimension(500,70));
+
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+        b3.addActionListener(this);
+        b4.addActionListener(this);
+        b5.addActionListener(this);
+    }
+
+    /**
+     * 设置文本域
+     */
+    public void setTextField(){
+        Font font1=new Font("黑体",Font.BOLD,60);
+        Font font2=new Font("黑体",Font.BOLD,35);
+        Font font3=new Font("黑体",Font.BOLD,35);
+        Font font4=new Font("黑体",Font.BOLD,35);
+        MatteBorder border4
+                =new MatteBorder(4,4,4,4,new Color(15, 3, 62));
+        f1=new JTextField(" 欢迎进入电话查询系统");
+        f2=new JTextField("---------查询结果--------");
+
+        textField=new JTextField();
+        Textnum=new JTextField();
+        Textname=new JTextField();
+        Textadress=new JTextField();
+        Tadress=new JTextField("请输入地址");
+        Tname=new JTextField("请输入姓名");
+        Tnum=new JTextField("请输入电话号码");
+
+
+        result=new JTextField();
+        result.setPreferredSize(new Dimension(700,75));
+        result.setBackground(new Color(143, 183, 217));
+        result.setBorder(border4);
+        result.setFont(font4);
+
+        f1.setPreferredSize(new Dimension(700,70));
+        f1.setBackground(new Color(76, 182, 194));
+        f1.setFont(font1);
+        MatteBorder border=
+                new MatteBorder(5,5,5,5,new Color(15, 3, 62));
+        f1.setBorder(border);
+        f1.setEnabled(false);
+
+
+        f2.setPreferredSize(new Dimension(500,70));
+        f2.setFont(font2);
+        f2.setBackground(new Color(95, 132, 208));
+        f2.setBorder(border);
+        f2.setEnabled(false);
+
+        textField.setPreferredSize(new Dimension(700,75));
+        textField.setFont(font3);
+        textField.setBackground(new Color(87, 158, 217));
+
+        textField.setBorder(border4);
+    }
+
+    /**
+     * about buttons TODO action of order
+     * @param e
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(b1==e.getSource()){
+            PhoneNumber = textField.getText();
+            SearchByNum();
+            System.out.println("给定电话号码为："+PhoneNumber+"\n"
+                    +"搜索到的号码数据为：");
+            haxi.SerchKey_Phone(PhoneNumber);
+            result.setText(haxi.Res_phone);
+        }
+
+        if(b2==e.getSource()){
+            Name = textField.getText();
+            SearchByName();
+            System.out.println("给定姓名为："+Name+"\n"
+                    +"搜索到的姓名数据为：");
+            haxi.SerchKey_Name(Name);
+            result.setText(haxi.Res_name);
+        }
+
+        if(b3==e.getSource()){
+            Address = textField.getText();
+            System.out.println("给定地址为："+Address+"\n"
+                    +"搜索到的地址数据为：");
+            haxi.SerchKey_Address(Address);
+            result.setText(haxi.Res_address);
+        }
+
+        if(b4==e.getSource()){
+            boolean flag=true;
+            String name=JOptionPane.showInputDialog("请输入姓名");
+            if(haxi.SerchKey_Name(name))
+                JOptionPane.showMessageDialog(null,"该名字已经存在",
+                        "注意",JOptionPane.WARNING_MESSAGE);
+                flag=false;
+            String phone=JOptionPane.showInputDialog("请输入电话号码");
+            if(haxi.SerchKey_Phone(phone)){
+                JOptionPane.showMessageDialog(null,"该号码已经存在",
+                        "注意",JOptionPane.WARNING_MESSAGE);
+                flag=false;
+            }
+            String address=JOptionPane.showInputDialog("请输入地址");
+            if(haxi.SerchKey_Address(address)){
+                JOptionPane.showMessageDialog(null,"该地址已经存在",
+                        "注意",JOptionPane.WARNING_MESSAGE);
+                flag=false;
+            }
+            if(flag)
+            {
+                haxi.record[haxi.len].name=name;
+                haxi.record[haxi.len].phoneNumber=phone;
+                haxi.record[haxi.len].address=address;
+                haxi.len++;
+                haxi.CreateHashTable_Phone();
+                haxi.CreateHashTable_Name();
+                haxi.CreateHashTable_Address();
+            }
+        }
+
+        if(b5==e.getSource()){
+            System.exit(0);
+        }
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.drawImage(back,0,0,back.getWidth(),back.getHeight(),null);
+    }
+}
+
+/**
+ * 总体界面
+ */
+public class MyTextField{
+    public JFrame frame;
+    public BufferedImage ICON;
+
+    public MyTextField(){
+        try{
+            ICON=ImageIO.read(MyTextField.class.getResource("icon.png"));
+        }catch (IOException e){}
+        frame=new JFrame("电话查询系统");
+        frame.setSize(1000,751);
+        frame.setIconImage(ICON);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+    public static void main(String[]args){
+       GraPanel graPanel=new GraPanel();
+       graPanel.haxi.InitialTable();
+       MyTextField textField=new MyTextField();
+       textField.frame.add(graPanel);
+    }
+}
